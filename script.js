9b81aaf88411fed1b2b0055d0a4375d3f8c8866a
cyanryann <209932180+cyanryann@users.noreply.github.com>
2025-08-30 23:35:53 -0400

    operations working

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ let second = 0;
 let operator = "+";
 let answer = 0;
 let lastAnswer = 0;
+let firstNumberSelected = false;
+let completeOperation = false;
 const output = document.querySelector(".output");
+const operators = document.querySelectorAll(".operations button");
+const numbers = document.querySelectorAll(".numbers button");
 
 function add(a,b) {
     return a + b;
@@ -17,35 +21,99 @@ function multiply(e, f) {
 function divide(g, h) {
     return g/h;
 }
-function operation() {
-    if (operator == "+")
+function operate() {
+    if (firstNumberSelected && output.innerHTML != "")
     {
-        answer = add(first, second);
-    }
-    else if (operator == "-")
-    {
-        answer = subtract(first, second);
-    }
-    else if (operator == "*")
-    {
-        answer = multiply(first, second);
-    }
-    else if (operator == "/")
-    {
-        if (second == 0)
+        second = parseFloat(output.innerHTML);
+        if (operator == "+")
         {
-            answer = 0;
-            output.innerHTML = "ERROR: Divide by Zero";
+            answer = add(first, second);
+        }
+        else if (operator == "-")
+        {
+            answer = subtract(first, second);
+        }
+        else if (operator == "*")
+        {
+            answer = multiply(first, second);
+        }
+        else if (operator == "/")
+        {
+            if (second == 0)
+            {
+                answer = 0;
+                output.innerHTML = "ERROR: Divide by Zero";
+            }
+            else
+            {
+                answer = divide(first, second);
+            }
+        
         }
         else
         {
-            answer = divide(first, second);
+            output.innerHTML = "uhhh what the heck";
         }
+        lastAnswer = answer;
+        
+        output.innerHTML = first + " " + operator + " " + second + " = " + answer;
+        firstNumberSelected = false;
+        first = 0;
+        second = 0;
+        completeOperation = true;
+    }
+    
+}
+function changeOperator(op) {
+    console.log("operation ran")
+    if (!firstNumberSelected && output.innerHTML != "")
+    {
+        operator = op;
+        console.log("operation chosen is " + operator);
+        first = parseFloat(output.innerHTML);
+        console.log("first number is " + first);
+        output.innerHTML = "";
+        firstNumberSelected = true;
+    }
+}
+operators.forEach((button) => {
+    
+    if (button.id != "solve")
+    {
+        console.log("adding listener " + button.innerHTML);
+        button.addEventListener("click", () => changeOperator(button.innerHTML));
+    }
+    else {
+        button.addEventListener("click", () => operate());
+    }
+})
+numbers.forEach((button) => {
+    if (button.innerHTML == "CLR")
+    {
+        button.addEventListener("click", () =>{
+            output.innerHTML = "";
+        })
         
     }
-    else
+    else if (button.innerHTML == "ANS")
     {
-        output.innerHTML = "uhhh what the heck";
+        button.addEventListener("click", () => {
+            if (completeOperation)
+            {
+                output.innerHTML = "";
+                completeOperation = false;
+            }
+            output.innerHTML = lastAnswer;
+        })
     }
-    lastAnswer = answer;
-}
+    else {
+        button.addEventListener("click", () => {
+            if (completeOperation)
+            {
+                output.innerHTML = "";
+                completeOperation = false;
+            }
+            output.innerHTML += button.innerHTML;
+        })
+    }
+})
